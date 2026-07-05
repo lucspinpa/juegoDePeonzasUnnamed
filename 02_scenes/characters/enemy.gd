7eb1e8e3c2f9d02f5710a720c7208889ce754a73
Jaco <jacooutofcontext@gmail.com>
2026-07-05 18:46:20 +0200

    Updated Player Movement and Enemy script
    - Player Movement is closer to what we wanted I guess.
    - Enemies will only target the player (the old script is still included as comment).
    - Changed the number of enemies on the testing area to just one.

--- 02_scenes/characters/enemy.gd
+++ 02_scenes/characters/enemy.gd
@@ -26,21 +26,24 @@ func _physics_process(delta):
 	move_and_slide()
 
 func find_target():
-	target_array.append(get_tree().get_first_node_in_group("player"))
-	for i in get_tree().get_nodes_in_group("enemies"):
-		target_array.append(i)
+	current_target = get_tree().get_first_node_in_group("player")
 	
-	var random_number = randi_range(0,4)
 	
-	# There's a 60% chance for an enemy to pick the player as its target.
-	if random_number > 1:
-		current_target = target_array[0]
-	else:
-		for i in target_array:
-			if i !=target_array[0] and i != self and i != null:
-				current_target = i
-				break
-			else:
-				continue
-	
-	print(str(self) + " target is " + str(current_target))
+	#target_array.append(get_tree().get_first_node_in_group("player"))
+	#for i in get_tree().get_nodes_in_group("enemies"):
+		#target_array.append(i)
+	#
+	#var random_number = randi_range(0,4)
+	#
+	## There's a 60% chance for an enemy to pick the player as its target.
+	#if random_number > 1:
+		#current_target = target_array[0]
+	#else:
+		#for i in target_array:
+			#if i !=target_array[0] and i != self and i != null:
+				#current_target = i
+				#break
+			#else:
+				#continue
+	#
+	#print(str(self) + " target is " + str(current_target))
--- 02_scenes/characters/player.gd
+++ 02_scenes/characters/player.gd
@@ -4,32 +4,25 @@ extends CharacterBody3D
 
 const JUMP_VELOCITY = 4.5
 
-var speed = 200.0
+var max_speed = 2.0
+var acceleration = 5.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed * delta
-		velocity.z = direction.z * speed * delta
+	if input_dir:
+		# If there is an input, calculate velocity.
+		# Adds acceleration to the current velocity each frame. Clamps it not to exceed max speed.
+		velocity.x = clamp(velocity.x + acceleration * delta * input_dir.x, -max_speed, max_speed)
+		velocity.z = clamp(velocity.z + acceleration * delta * input_dir.y, -max_speed, max_speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+		velocity.z = move_toward(velocity.z, 0, acceleration * delta)
 
 	var rotation_multiplier = clamp(
 			((abs(velocity.x) + abs(velocity.z)) / 100),
 			0.0, 1.0)
 	mesh.rotation.y += 9 * rotation_multiplier
-
 
 	move_and_slide()
